fb2bb56c51753ee4773a6251907d665b6a6b6d11
Rifat Ismailov <refaazer@gmail.com>
2022-11-28 19:58:12 +0200

    Initial commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,9 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello My friend" +
+                "\n"+
+                "How are You?");
+    }
+}
