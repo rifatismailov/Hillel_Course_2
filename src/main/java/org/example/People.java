033a8b48a0a55b830cc913b640e9856fe29874d5
Rifat Ismailov <refaazer@gmail.com>
2022-12-02 13:24:58 +0200

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	README.md
    add git ignore

--- src/main/java/org/example/People.java
+++ src/main/java/org/example/People.java
@@ -10,6 +10,9 @@ public class People {
     public int getAge(){
         return age;
     }
+    public String getName(){
+        return name;
+    }
     @Override
     public String toString(){
         return name+" "+age;
