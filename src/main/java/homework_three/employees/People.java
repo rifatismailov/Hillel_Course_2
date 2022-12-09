fc796f434c257af62b233a46f680e0e8a1dc6aa2
Rifat Ismailov <refaazer@gmail.com>
2022-12-09 17:47:33 +0200

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	README.md
    add git ignore

---
+++ src/main/java/homework_three/employees/People.java
@@ -0,0 +1,15 @@
+package homework_three.employees;
+
+public class People {
+    public static void main(String[] args) {
+        Employees[] peoples=new Employees[]{
+                new Employees("Rifat Ismailov","Engineer","refaazer@gmail.com","+380666666666",32),
+                new Employees("Max Ibragimov","Engineer","max23511@gmail.com","+380666777777",28),
+                new Employees("Alex Popov","Engineer","alxpop23@gmail.com","+380677776666",30),
+                new Employees("Natasha Sgaydachna","Secretary","natsgay1995@gmail.com","+380665567666",25)
+        };
+        for (Employees employees:peoples){
+            System.out.println(employees);
+        }
+    }
+}
