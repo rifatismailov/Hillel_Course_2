579255693922d948b712930af0b2f7d866cd14af
Rifat Ismailov <refaazer@gmail.com>
2022-12-11 19:08:08 +0200

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	README.md
    add git ignore
    as

--- src/main/java/homework_foor/Main.java
+++ src/main/java/homework_foor/Main.java
@@ -2,28 +2,34 @@ package homework_foor;
 
 public class Main {
     public static void main(String[] args) {
-        Animal[] animals = new Animal[]{ new Dog("Бобик"), new Cat("мурка"),new Animal("Лошадь"),
-                new Animal("Корова"),new Animal("Баран"),new Dog("Рекс")
+        Animal[] animals = new Animal[]{new Dog("Бобик"), new Cat("мурка"), new Animal("Лошадь"),
+                new Animal("Корова"), new Animal("Баран"), new Dog("Рекс")
         };
-        int anim=0;
-        int dog=0;
-        int cat=0;
-        for (Animal animal:animals){
-            animal.Ran(150);
-            animal.Swim(5);
-
-            if(animal.getAnimal().equals("Animal")){
+        int anim = 0;
+        int dog = 0;
+        int cat = 0;
+        for (Animal animal : animals) {
+            if (animal.getAnimal().equals("Animal")) {
                 anim++;
+                animal.Ran(230);
+                animal.Swim(15);
+                System.out.println();
             }
-            if(animal.getAnimal().equals("Dog")){
+            if (animal.getAnimal().equals("Dog")) {
                 dog++;
+                animal.Ran(250);
+                animal.Swim(8);
+                System.out.println();
             }
-            if(animal.getAnimal().equals("Cat")){
+            if (animal.getAnimal().equals("Cat")) {
                 cat++;
+                animal.Ran(210);
+                animal.Swim(5);
+                System.out.println();
             }
         }
-        System.out.println("кількість собак "+dog);
-        System.out.println("кількість кошек "+cat);
-        System.out.println("кількість інших тварин "+anim);
+        System.out.println("кількість собак " + dog);
+        System.out.println("кількість кошек " + cat);
+        System.out.println("кількість інших тварин " + anim);
     }
 }
