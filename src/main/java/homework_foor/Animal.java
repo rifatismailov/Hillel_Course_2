1c2e205d08af31b9d4694aa66e4f283f6fbbf35b
Rifat Ismailov <refaazer@gmail.com>
2022-12-11 19:21:26 +0200

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	README.md
    add git ignore
    as

--- src/main/java/homework_foor/Animal.java
+++ src/main/java/homework_foor/Animal.java
@@ -1,27 +1,34 @@
 package homework_foor;
-/**1. Створити порожній проект в IntelliJ
- 2. Створити класи Собака та Кіт з успадкуванням від класу Тварини.
- 3. Всі тварини можуть бігти та плисти. Як параметр кожному методу передається довжина перешкоди.
- Результатом виконання дії буде друк у консоль. (Наприклад, dogBobik.run(150); -> 'Бобік пробіг 150 м');
- 4. Кожна тварина має обмеження на дії (біг: кіт 200 м., собака 500 м.; плавання: кіт не вміє плавати, собака 10 м.).
- 5. * Додати підрахунок створених котів, собак та тварин.*/
+
+/**
+ * 1. Створити порожній проект в IntelliJ
+ * 2. Створити класи Собака та Кіт з успадкуванням від класу Тварини.
+ * 3. Всі тварини можуть бігти та плисти. Як параметр кожному методу передається довжина перешкоди.
+ * Результатом виконання дії буде друк у консоль. (Наприклад, dogBobik.run(150); -> 'Бобік пробіг 150 м');
+ * 4. Кожна тварина має обмеження на дії (біг: кіт 200 м., собака 500 м.; плавання: кіт не вміє плавати, собака 10 м.).
+ * 5. * Додати підрахунок створених котів, собак та тварин.
+ */
 public class Animal {
     private String name;
 
     public Animal(String name) {
-    this.name=name;
+        this.name = name;
     }
-    public String getName(){
+
+    public String getName() {
         return name;
     }
-    public void Ran(int ran){
-        System.out.println(name + " пробіг "+ran +" м");
+
+    public void Ran(int ran) {
+        System.out.println(name + " пробіг " + ran + " м");
     }
-    public void Swim(int swim){
-        System.out.println(name + " проплив "+swim+" м");
+
+    public void Swim(int swim) {
+        System.out.println(name + " проплив " + swim + " м");
 
     }
-    public String getAnimal(){
+
+    public String getAnimal() {
         return "Animal";
     }
 
