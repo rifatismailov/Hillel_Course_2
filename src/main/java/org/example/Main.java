36a86ba04c7db7702b4276b26b1e19ff1460c33a
Rifat Ismailov <refaazer@gmail.com>
2022-12-11 17:23:21 +0200

    Merge remote-tracking branch 'origin/main'
    add git ignore
    as

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -23,10 +23,11 @@ public class Main {
         peopleList.add(new People("Konan",26));
         peopleList.add(new People("Yorik",26));
         peopleList.add(new People("Rifat",26));
-        peopleList.add(new People("Abba",26));
         peopleList.add(new People("Stuart",26));
         peopleList.add(new People("Barbara",26));
         peopleList.add(new People("Abba",26));
+        peopleList.add(new People("Sem",26));
+        peopleList.add(new People("Abraham",26));
         peopleList.add(new People("Sem",26));
         peopleList.add(new People("Ahmed",26));
         for(People people:peopleList){
