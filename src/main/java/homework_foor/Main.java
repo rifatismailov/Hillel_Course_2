2742fbd568cec9f6f26c290c295ae85dd48a847c
refatIzi <46138835+refatIzi@users.noreply.github.com>
2022-12-11 20:15:01 +0200

    Homework 5 (#9)
    * Merge remote-tracking branch 'origin/main'
    
    # Conflicts:
    #	README.md
    add git ignore
    as
    
    * Merge remote-tracking branch 'origin/main'
    
    # Conflicts:
    #	README.md
    add git ignore
    as
    
    * Merge remote-tracking branch 'origin/main'
    
    # Conflicts:
    #	README.md
    add git ignore
    as
    
    * Merge remote-tracking branch 'origin/main'
    
    # Conflicts:
    #	README.md
    add git ignore
    as
    
    * Merge remote-tracking branch 'origin/main'
    
    # Conflicts:
    #	README.md
    add git ignore
    as
    
    * Revert "Merge remote-tracking branch 'origin/main'"
    
    This reverts commit 72994f66d9f063b57eec199a6f3e6f449adcca25.
    
    * change name methods

---
+++ src/main/java/homework_foor/Main.java
@@ -0,0 +1,35 @@
+package homework_foor;
+
+public class Main {
+    public static void main(String[] args) {
+        Animal[] animals = new Animal[]{new Dog("Бобик"), new Cat("мурка"), new Animal("Лошадь"),
+                new Animal("Корова"), new Animal("Баран"), new Dog("Рекс")
+        };
+        int anim = 0;
+        int dog = 0;
+        int cat = 0;
+        for (Animal animal : animals) {
+            if (animal.getAnimal().equals("Animal")) {
+                anim++;
+                animal.run(230);
+                animal.swim(15);
+                System.out.println();
+            }
+            if (animal.getAnimal().equals("Dog")) {
+                dog++;
+                animal.run(250);
+                animal.swim(8);
+                System.out.println();
+            }
+            if (animal.getAnimal().equals("Cat")) {
+                cat++;
+                animal.run(210);
+                animal.swim(5);
+                System.out.println();
+            }
+        }
+        System.out.println("кількість собак " + dog);
+        System.out.println("кількість кошек " + cat);
+        System.out.println("кількість інших тварин " + anim);
+    }
+}
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -13,10 +13,9 @@ public class Main {
         peopleList.add(new People("Max",33));
         peopleList.add(new People("Lesia",28));
         peopleList.add(new People("Inna",25));
+
         for(People people:peopleList){
             System.out.println(people);
         }
-
-
     }
 }
