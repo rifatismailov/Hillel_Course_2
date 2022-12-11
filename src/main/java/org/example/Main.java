c30299b93625ad73e6c433288646295a2693c132
Rifat Ismailov <refaazer@gmail.com>
2022-12-11 17:58:35 +0200

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	README.md
    add git ignore
    as

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -13,20 +13,6 @@ public class Main {
         peopleList.add(new People("Max",33));
         peopleList.add(new People("Lesia",28));
         peopleList.add(new People("Inna",25));
-        peopleList.add(new People("Nadia",26));
-        peopleList.add(new People("Andriy",26));
-        peopleList.add(new People("Slavik",26));
-        peopleList.add(new People("Vlad",26));
-        peopleList.add(new People("Oleg",26));
-        peopleList.add(new People("Voldemar",26));
-        peopleList.add(new People("Alisa",26));
-        peopleList.add(new People("Konan",26));
-        peopleList.add(new People("Yorik",26));
-        peopleList.add(new People("Rifat",26));
-        peopleList.add(new People("Lara",26));
-        peopleList.add(new People("Emili",26));
-        peopleList.add(new People("Barbara",26));
-        peopleList.add(new People("Esma",26));
         for(People people:peopleList){
             System.out.println(people);
         }
