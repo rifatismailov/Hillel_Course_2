89dddf8db21f7f5e37bcbc85bd9d0e84b526c4d4
Rifat Ismailov <refaazer@gmail.com>
2022-12-20 22:19:13 +0200

    Seven homework.

--- src/main/java/homework/seven/ArrayValueCalculator.java
+++ src/main/java/homework/seven/ArrayValueCalculator.java
@@ -4,7 +4,7 @@ public class ArrayValueCalculator {
     String[][] array = new String[4][4];
 
     public int doCalc(String[][] array) throws ArraySizeException, ArrayDataException {
-        int suma = 0;
+        int sum = 0;
         this.array = array;
         if (this.array.length != 4) {
             throw new ArraySizeException("["+this.array.length+"]");
@@ -16,15 +16,15 @@ public class ArrayValueCalculator {
                     for (int j = 0; j < 4; j++) {
                         System.out.print(array[i][j] + " ");
                         try {
-                            suma += Integer.parseInt(array[i][j]);
+                            sum += Integer.parseInt(array[i][j]);
                         } catch (NumberFormatException e) {
                             throw new ArrayDataException(i, j, array[i][j]);
                         }
                     }
                 }
-                System.out.println("> " + suma);
+                System.out.println("> " + sum);
             }
         }
-        return suma;
+        return sum;
     }
 }
