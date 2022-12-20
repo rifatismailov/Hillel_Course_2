00cfae5a760b7a1521746e92a3e3ac04463f94bf
Rifat Ismailov <refaazer@gmail.com>
2022-12-20 21:34:31 +0200

    Seven homework.

--- src/main/java/homework/seven/Main.java
+++ src/main/java/homework/seven/Main.java
@@ -5,11 +5,10 @@ public class Main {
         ArrayValueCalculator calculator=new ArrayValueCalculator();
         String[][] array={
                 {"2","5","1","8"},
-                {"4","2","5","6"},
+                {"4","2","&","6"},
                 {"3","9","5","34"},
-                {"5","5","3","6","8"},
+                {"5","5","3","6"},
                // {"5","6","7","6"}
-
         };
         System.out.println("Answer "+calculator.doCalc(array));
     }
